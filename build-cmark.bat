d6dbf848eacef5cb853ea7e13db2239f8796cc69
Nathan Lanza <nathan@lanza.io>
2019-03-08 20:12:51 -0800

    'IDK'

--- build-cmark.bat
+++ build-cmark.bat
@@ -3,10 +3,10 @@ mkdir %builddir%
 
 pushd %builddir%
 cmake -G Ninja^
-  -DCMAKE_INSTALL_PREFIX="C:\swift"^
   -DCMAKE_BUILD_TYPE=Release^
   -DCMAKE_C_COMPILER=cl^
   -DCMAKE_CXX_COMPILER=cl^
+  -DCMAKE_INSTALL_PREFIX="C:\swift"^
   S:\cmark
 popd
 
